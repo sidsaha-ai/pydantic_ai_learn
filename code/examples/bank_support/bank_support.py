33299f967ba18d17f362bed61cc08a426d3500db
Sid Saha <siddharthsaha@Siddharths-MacBook-Pro.local>
2024-12-11 16:21:11 +0530

    using groq

--- code/examples/bank_support/bank_support.py
+++ code/examples/bank_support/bank_support.py
@@ -37,7 +37,7 @@ class DatabaseConn:
         Returns the customer's currency based on the customer ID.
         """
         if customer_id == 123:
-            return 'Rs.'
+            return '$'
 
         return None
 
@@ -102,18 +102,19 @@ m = LLMModel()
 
 # for groq
 m.model_type = 'groq'
-m.groq_model_name = 'llama-3.3-70b-versatile'
+m.groq_model_name = 'llama3-groq-70b-8192-tool-use-preview'
 
 llm_model = m.fetch_model()
 
 support_agent = Agent(
     llm_model,
     deps_type=SupportDependencies,
-    # result_type=SupportResult,  # NOTE: this does not work, prompting works better.
+    result_type=SupportResult,
     system_prompt=(
         'You are a support agent in our bank, provide the the customer '
         'some support advice as per their request and judge the risk level of their query. '
         "When reporting the customer's balance, report using the customer's preferred currency. "
+        "Reply using the customer's name"
     ),
 )
 
@@ -125,20 +126,7 @@ async def add_customer_name(ctx: RunContext[SupportDependencies]) -> str:
     """
     deps: SupportDependencies = ctx.deps
     customer_name = await deps.db.customer_name(customer_id=deps.customer_id)
-    return f'The name of the customer is {customer_name}'
-
-
-@support_agent.system_prompt
-async def return_json() -> str:
-    """
-    Constructs the return type that the LLM should return.
-    """
-    prompt = (
-        f'Please return ONLY a JSON string like this example:  {generate_json(SupportResult)}. '
-        'If any field in the JSON is not applicable, return its value as `null`. '
-        "Make sure to include the customer's name in the final support_advice field. "
-    )
-    return prompt
+    return f'The name of the customer is "{customer_name}". Always include the name of the customer in the reply.'
 
 
 @support_agent.tool
@@ -160,7 +148,6 @@ async def customer_preffered_currency(ctx: RunContext[SupportDependencies]) -> s
     """
     deps: SupportDependencies = ctx.deps
     currency = await deps.db.customer_currency(customer_id=deps.customer_id)
-    print(f'======= Currency: {currency} ======')
     return f'{currency}'
 
 
@@ -173,8 +160,7 @@ async def main(input_text: str, customer_id: int) -> None:
     )
     result = await support_agent.run(input_text, deps=deps)
 
-    res = SupportResult.model_validate_json(result.data)
-    print(res)
+    print(result.data)
 
 
 if __name__ == '__main__':
