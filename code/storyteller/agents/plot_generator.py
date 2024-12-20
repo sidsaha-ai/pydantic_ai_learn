347423eb5c450b1f5a4a52b0b26dbdcd7c55d9e5
Sid Saha <siddharthsaha@Siddharths-MacBook-Pro.local>
2024-12-20 21:11:17 +0530

    starting work on high-level plot generator

---
+++ code/storyteller/agents/plot_generator.py
@@ -0,0 +1,70 @@
+"""
+This is the Plot Generator agent that generates the high level plot of the story.
+"""
+import asyncio
+from dataclasses import dataclass
+
+import logfire
+from pydantic import BaseModel, Field
+from pydantic_ai import Agent, RunContext
+from utils.llm_model import LLMModel
+
+logfire.configure(console=False)
+
+
+@dataclass
+class Deps:
+    """
+    The dependencies needed by this agent.
+    """
+    theme: str  # the high level theme of the restaurant
+
+
+class Result(BaseModel):
+    """
+    The data model produced as the final result of this agent.
+    """
+    characters: list[str] = Field(description='The characters of the story.')
+    setting: str = Field(description="The story's general setting")
+    inciting_incident: str = Field(description='The inciting incident in the plot.')
+    action: str = Field(description='The action to the incident.')
+    climax: str = Field(description='The climax of the plot.')
+    resolution: str = Field(description='The resolution of the plot of the story.')
+
+
+m = LLMModel()
+m.model_type = 'groq'
+m.groq_model_name = 'llama3-groq-70b-8192-tool-use-preview'
+
+agent = Agent(
+    m.fetch_model(),
+    deps_type=Deps,
+    result_type=Result,
+    system_prompt=(
+        'You are on the plot generater of a story based on a given theme. '
+        'Your job is to generate a high-level plot of the story. '
+        'You should output the characters of the story. The number of characters in the story should be between 3 and 7. '
+        'You should output the general setting where the story takes place, an inciting incident that would happen in the story, '
+        'an action to the incident, a climax, and a resolution to the story. '
+        'Remember, your job is to only generate a high-level plot of the story.'
+        'The goal is to create a high-level plot that is engaging, realistic, and entertaining. '
+    ),
+)
+
+@agent.system_prompt
+def theme_prompt(ctx: RunContext[Deps]) -> str:
+    """
+    Add the theme as a system prompt.
+    """
+    deps: Deps = ctx.deps
+    return f'The theme of the story is a {deps.theme}'
+
+
+if __name__ == '__main__':
+    theme: str = 'murder mystery'
+    theme: str = 'romantic love story'
+    theme: str = 'thriller'
+    theme: str = 'drama'
+
+    res = agent.run_sync("Let's start!", deps=Deps(theme=theme))
+    print(res.data)
